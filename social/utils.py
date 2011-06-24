864cd95293550c907ec5834cb5d0c2dce6a109c7
Jacco Flenter <github@putpixel.nl>
2011-06-24 11:14:15 +0200

    generate_password added

--- social/utils.py
+++ social/utils.py
@@ -12,3 +12,30 @@ def get_genus():
     genus = GenusApi(consumer, "2.0")
     
     return genus
+
+
+import sys
+from random import Random
+
+def generate_password(passwordLength=8, alternate_hands=True):
+    """Generate a random password of passwordLength length and possibly with
+    alternating characters for left/right hand
+    """
+    rng = Random()
+    
+    righthand = '23456qwertasdfgzxcvbQWERTASDFGZXCVB'
+    lefthand = '789yuiophjknmYUIPHJKLNM'
+    allchars = righthand + lefthand
+    
+    password = ""
+    for i in range(passwordLength):
+        if not alternate_hands:
+            password += rng.choice(allchars)
+        else:
+            if i%2:
+                password += rng.choice(lefthand)
+            else:
+                password += rng.choice(righthand)
+
+    return password
+
